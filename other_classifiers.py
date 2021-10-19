255494f67ce51ab192c8d806b2a2eef06a7ac2b0
Chua Zi Heng <chuaziheng98@gmail.com>
2021-10-19 19:53:36 +0800

    add log loss

--- other_classifiers.py
+++ other_classifiers.py
@@ -1,6 +1,4 @@
 import argparse
-import numpy as np
-from numpy.core.numeric import cross
 import pandas as pd
 
 from sklearn.metrics import make_scorer, accuracy_score, log_loss, classification_report
@@ -19,9 +17,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 parser = argparse.ArgumentParser()
 parser.add_argument('--filename', "-f", default="iris", help="Dataset name")
 
-class proba_logreg(LogisticRegression):
-    def predict(self, X):
-        return LogisticRegression.predict_proba(self, X)
+
 
 def preprocess_data(dataset):
     # le = preprocessing.LabelEncoder()
@@ -38,6 +34,13 @@ def classification_report_with_accuracy_score(y_true, y_pred):
     predictedclass.extend(y_pred)
     return accuracy_score(y_true, y_pred)
 
+def get_log_loss(y_true, y_pred):
+    return log_loss(y_true, y_pred)
+
+SCORE_MODELS = {
+    'accuracy': make_scorer(classification_report_with_accuracy_score),
+    'log_loss': make_scorer(log_loss,  needs_proba=True)
+}
 
 if __name__ == "__main__":
 
@@ -45,14 +48,15 @@ if __name__ == "__main__":
     print('Dataset: ', args.filename)
     X, y = preprocess_data(args.filename)
 
+    cv = 3 if args.filename == 'glass' else 10
     print(f"========================== Decision Tree ==========================")
     originalclass = []
     predictedclass = []
     dectree = DecisionTreeClassifier(random_state=2)
     dectree.fit(X, y.values.ravel())
-    dectree_results = cross_validate(dectree, X, y.values.ravel(), cv=10, scoring=make_scorer(classification_report_with_accuracy_score))
-
-    print("Average Accuracy: ", dectree_results['test_score'].mean())
+    dectree_results = cross_validate(dectree, X, y.values.ravel(), cv=cv, scoring=SCORE_MODELS)
+    print("Average Accuracy: ", dectree_results['test_accuracy'].mean())
+    print("Average Log Loss: ", dectree_results['test_log_loss'].mean())
     print('Classification report: ')
     print(classification_report(originalclass, predictedclass))
 
@@ -63,24 +67,23 @@ if __name__ == "__main__":
     predictedclass = []
     rforest = RandomForestClassifier(random_state=2)
     rforest.fit(X, y.values.ravel())
-    rf_results = cross_validate(rforest, X, y.values.ravel(), cv=10, scoring=make_scorer(classification_report_with_accuracy_score))
-    print("Average Accuracy: ", rf_results['test_score'].mean())
+    rf_results = cross_validate(rforest, X, y.values.ravel(), cv=cv, scoring=SCORE_MODELS)
+    print("Average Accuracy: ", rf_results['test_accuracy'].mean())
+    print("Average Log Loss: ", rf_results['test_log_loss'].mean())
     print('Classification report: ')
     print(classification_report(originalclass, predictedclass))
-    # print("Average Micro F1 Score: ",f1_score(originalclass, predictedclass, average='micro'))
-    # print("Average Macro F1 Score: ",f1_score(originalclass, predictedclass, average='macro'))
-    # print("Average Weighted F1 Score: ",f1_score(originalclass, predictedclass, average='weighted'))
 
     print()
     print(f"========================== SVM ==========================")
 
     originalclass = []
     predictedclass = []
-    svm_model=SVC(gamma='scale')
+    svm_model=SVC(gamma='scale', probability=True)
     svm_model.fit(X, y.values.ravel())
-    svm_results = cross_validate(svm_model, X, y.values.ravel(), cv=10, scoring=make_scorer(classification_report_with_accuracy_score))
+    svm_results = cross_validate(svm_model, X, y.values.ravel(), cv=cv, scoring=SCORE_MODELS)
 
-    print("Average Accuracy: ", svm_results['test_score'].mean())
+    print("Average Accuracy: ", svm_results['test_accuracy'].mean())
+    print("Average Log Loss: ", svm_results['test_log_loss'].mean())
     print('Classification report: ')
     print(classification_report(originalclass, predictedclass))
 
@@ -92,15 +95,8 @@ if __name__ == "__main__":
     gnb = GaussianNB()
     gnb.fit(X, y.values.ravel())
 
-    nb_results = cross_validate(gnb, X, y.values.ravel(), cv=10, scoring=make_scorer(classification_report_with_accuracy_score))
-    # print(len(originalclass))
-    # print(len(probas))
-    # print(originalclass)
-    # probas = cross_val_predict(proba_logreg(), X, y.values.ravel(), cv=10)
-    # print(probas)
-    # log_loss_score = log_loss(originalclass, probas)
-    # print(log_loss_score)
-    # print(len(predictedclass))
-    print("Average Accuracy: ", nb_results['test_score'].mean())
+    nb_results = cross_validate(gnb, X, y.values.ravel(), cv=cv, scoring=SCORE_MODELS)
+    print("Average Accuracy: ", nb_results['test_accuracy'].mean())
+    print("Average Log Loss: ", nb_results['test_log_loss'].mean())
     print('Classification report: ')
     print(classification_report(originalclass, predictedclass))
